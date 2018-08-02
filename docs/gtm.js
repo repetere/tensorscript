e696367f362d9261e66620805ea67c1b84ff56fb
yaw joseph etse <yaw.etse@gmail.com>
2018-08-02 15:02:47 -0400

    Adding google analytics

--- docs/gtm.js
+++ docs/gtm.js
@@ -1,5 +1,9 @@
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','UA-112697260-2');</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="//www.googletagmanager.com/gtag/js?id=UA-112697260-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'UA-112697260-2');
+  </script>
